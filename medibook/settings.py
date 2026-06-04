f40257e721e50a9a1b9bbf37feba0c20ae89b817
AbdElmoughit00 <rouhiabdelmoughit@gmail.com>
2026-06-04 23:27:08 +0100

    Change database configuration

--- medibook/settings.py
+++ medibook/settings.py
@@ -61,15 +61,14 @@ WSGI_APPLICATION = 'medibook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':  'django.db.backends.sqlite3',
-        'NAME':BASE_DIR / 'db.sqlite3',
-        'USER': os.getenv('DB_USER'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
